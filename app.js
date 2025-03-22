c55ed5842af43627599afe554d7549aa91be6b52
hoangdungmmary <lehoangdung2016@gmail.com>
2025-03-22 10:05:02 +0700

    push

--- app.js
+++ app.js
@@ -6,63 +6,65 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html; charset=UTF-8');
 
   const html = `
-    <html>
-    <head>
-      <title>Danh sách học viên</title>
-      <style>
-        table {
-          border-collapse: collapse;
-          width: 100%;
-        }
-        th, td {
-          border: 1px solid black;
-          padding: 8px;
-          text-align: center;
-        }
-        th {
-          background-color: #f2f2f2;
-        }
-      </style>
-    </head>
-    <body>
-      <h1>Danh sách học viên</h1>
-      <table>
-        <tr>
-          <th>MSHV</th>
-          <th>Họ tên</th>
-          <th>Ngày sinh</th>
-          <th>Quê quán</th>
-          <th>Email</th>
-          <th>SĐT</th>
-        </tr>
-        <tr>
-          <td>M2524003</td>
-          <td>Le Hoang Dung</td>
-          <td>01/01/2001</td>
-          <td>Cần Thơ</td>
-          <td>nguyenvana@example.com</td>
-          <td>0912345678</td>
-        </tr>
-        <tr>
+  <html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Danh sách học viên</title>
+    <style>
+      table {
+        border-collapse: collapse;
+        width: 100%;
+      }
+      th, td {
+        border: 1px solid black;
+        padding: 8px;
+        text-align: center;
+      }
+      th {
+        background-color: #f2f2f2;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Danh sách học viên</h1>
+    <table>
+      <tr>
+        <th>MSHV</th>
+        <th>Họ tên</th>
+        <th>Ngày sinh</th>
+        <th>Quê quán</th>
+        <th>Email</th>
+        <th>SĐT</th>
+      </tr>
+      <tr>
         <td>M2524003</td>
-        <td>Nguyen Van A</td>
+        <td>Lê Hoàng Dũng</td>
         <td>01/01/2001</td>
         <td>Cần Thơ</td>
         <td>nguyenvana@example.com</td>
         <td>0912345678</td>
       </tr>
-        <tr>
-          <td>...</td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </table>
-    </body>
-    </html>
-  `;
+      <tr>
+        <td>M2524004</td>
+        <td>Nguyễn Văn A</td>
+        <td>01/01/2001</td>
+        <td>Cần Thơ</td>
+        <td>nguyenvana@example.com</td>
+        <td>0912345678</td>
+      </tr>
+      <tr>
+        <td>...</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  </body>
+  </html>
+`;
+
 
   res.end(html);
 });
